94d3d4bee287909033731ce38eb58dd06a02e463
Daniil <dannyboykov@bk.ru>
2023-08-27 19:04:49 +0300

    Commit.

---
+++ src/Dzz.java
@@ -0,0 +1,5 @@
+public class Dzz {
+    public static void main(String[] args) {
+
+    }
+}
--- src/MapStream.java
+++ src/MapStream.java
@@ -12,10 +12,17 @@ public class MapStream {
         List<String> sr = map.entrySet().stream().flatMap((Map.Entry<String, String> entr) -> Stream.of((entr.getKey()), entr.getValue())).collect(Collectors.toList());
         List<Stud> studList = new ArrayList<>();
         studList.add(new Stud("msfa", "architector"));
-        studList.add(new Stud("safv", "programmer"));
+        studList.add(new Stud("msfasaf", "architector"));
+        studList.add(new Stud("safqfv", "programmer"));
+        studList.add(new Stud("safvrrw", "programmer"));
+        studList.add(new Stud("safffv", "programmer"));
         studList.add(new Stud("asflc", "builder"));
-        studList.add(new Stud("vasdf", "manager"));
+        studList.add(new Stud("35hsgte", "builder"));
+        studList.add(new Stud("3rds", "builder"));
+        studList.add(new Stud("v665sdf", "manager"));
         List<String> s = studList.stream().map((Stud se) -> se.getSpecialization()).sorted().collect(Collectors.toList());
-        System.out.println(s);
+        Map<String, List<Stud>> studMap = studList.stream().collect(Collectors.groupingBy(Stud::getSpecialization));
+        studMap.entrySet().stream().forEach((Map.Entry<String, List<Stud>> par) -> {
+            System.out.println(par.getKey() + " :" + par.getValue().size());});
     }
 }
