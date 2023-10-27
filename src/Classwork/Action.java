733867b6079c50340216d02e3ab218e122e20a57
Daniil <dannyboykov@bk.ru>
2023-10-27 19:54:38 +0300

    Commit.

--- src/Classwork/Action.java
+++ src/Classwork/Action.java
@@ -2,29 +2,46 @@ package Classwork;
 
 import java.io.File;
 import java.time.LocalTime;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.concurrent.RecursiveAction;
 
 
 public class Action extends RecursiveAction {
-    final int TIME = 5;
-    Action action = new Action();
+    final int TIME = 30;
+    String[] folders;
+
+    public Action(String[] folders) {
+        this.folders = folders;
+    }
 
     @Override
     protected void compute() {
-        if(){
-// создать конструктор для Action который принимает список папок,
-// в compute надо проверку на кол-во файлов в папках
-// , если файлов много распараллелить их в экшены.
+        if(folders.length == 1){
+            monitorFolder(folders[0]);
+        } else {
+            Action action = new Action(Arrays.copyOfRange(folders, 0, folders.length/2));
+            Action action1 = new Action(Arrays.copyOfRange(folders, folders.length/2, folders.length));
+            action.fork();
+            action1.fork();
+            action.join();
+            action.join();
         }
+
     }
 
     private void monitorFolder(String path){
+        String[] files;
+        Random random = new Random();
+        int number = random.nextInt();
         LocalTime lt = LocalTime.now();
         while(lt.plusSeconds(TIME).isAfter(LocalTime.now())){
+            //System.out.println("Works" + number);
             File file = new File(path);
-            String[] files = file.list((File file1, String name) -> file1.isFile());
-            if(files.length > 1){
+            files = file.list();
+            if(files.length >= 1){
                 System.out.println("files appeared");
+                break;
             }
         }
     }
--- src/HashMapDz.java
+++ src/HashMapDz.java
@@ -1,5 +1,3 @@
-import com.sun.jdi.IntegerValue;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
